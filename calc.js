7e6c465c871bd6857fbb5fa7b8b2af9504abbea6
Sam Hoffman <samdhoffman@gmail.com>
2017-10-06 12:00:56 -0400

    Initial commit

---
+++ calc.js
@@ -0,0 +1,7 @@
+var add = require('./add');
+var subtract = require('./subtract');
+var multiply = require('./multiply');
+
+console.log(add(2,3));
+console.log(subtract(10,2));
+console.log(multiply(10,10));
